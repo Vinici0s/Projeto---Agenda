e911341e8ddd6124da63e8d13b22255a79ff5eb4
Vinicios Albuquerque <viniciusmelo.art@gmail.com>
2022-09-26 18:04:11 -0300

    Cadastro de usuarios implementado

--- Agenda - padrão MVC/src/controllers/loginController.js
+++ Agenda - padrão MVC/src/controllers/loginController.js
@@ -1,3 +1,32 @@
+const { async } = require('regenerator-runtime');
+const Login = require('../models/LoginModel');
+
 exports.index = (request, response) => {
     response.render('login');
+};
+
+exports.register = async (request, response) => {
+
+    try{
+        const login = new Login(request.body);
+        await login.register();
+    
+        if(login.errors.length > 0){
+            request.flash('errors', login.errors);
+            request.session.save(function(){
+               return response.redirect('/login/index');
+            });
+            return;
+        }
+    
+        request.flash('success', 'Seu usuário foi criado com sucesso.');
+        request.session.save(function(){
+           return response.redirect('/login/index');
+        });
+
+    } catch(e){
+        console.log(e);
+        return response.render('404');
+    }
+    
 };
--- Agenda - padrão MVC/src/middlewares/middleware.js
+++ Agenda - padrão MVC/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (request, response, next) => {
-    response.locals.umaVariavelLocal = 'Local variable';
+    response.locals.errors = request.flash('errors');
+    response.locals.success = request.flash('success');
     next();
 };
 
