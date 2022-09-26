70a47c72eac27d76fc30d1e05e20139a2eef034c
Vinicios Albuquerque <viniciusmelo.art@gmail.com>
2022-09-26 15:07:26 -0300

    Projeto criado, template e arquivos necessarios

---
+++ Agenda - padrão MVC/src/middlewares/middleware.js
@@ -0,0 +1,16 @@
+exports.middlewareGlobal = (request, response, next) => {
+    response.locals.umaVariavelLocal = 'Local variable';
+    next();
+};
+
+exports.checkCsrfError = (error, request, response, next) => {
+    if(error) {
+        return response.render('404');
+    }
+    next();
+};
+
+exports.csrfMiddleware = (request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+};
