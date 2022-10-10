33b58a9731ba62b2c72f21e2840ae28bbfdf8dcd
Vinicios Albuquerque <viniciosmelo.js@gmail.com>
2022-10-10 14:47:26 -0300

    correção de bug

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(routes);
 
 app.on('Connected', () => {
     app.listen(3000, () => {
-        console.log('Acessar http://localhost:3000')
+        console.log('Acessar https://localhost:3000')
         console.log('Servidor executando na porta 3000');
     });
 })
